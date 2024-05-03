2dfca50f58b69202e72e3dcb7c55269741a0650f
Aakansh Yadav <132001207+iaakanshff@users.noreply.github.com>
2024-05-04 01:16:26 +0530

    Update gostyle.go

--- gostyle.go
+++ gostyle.go
@@ -1,240 +1,191 @@
 package gostyle
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// GoStyle represents the style library
-type GoStyle struct{}
+// gostyle represents the style library
+type gostyle struct{}
 
-// New creates a new instance of the GoStyle library
-func New() *GoStyle {
-	return &GoStyle{}
+// New creates a new instance of the gostyle library
+func New() *gostyle {
+	return &gostyle{}
 }
 
-// Define maps for colors, styles, effects, and log types
-var (
-	colorMap = map[string]int{
-		"black":     30,
-		"red":       31,
-		"green":     32,
-		"yellow":    33,
-		"blue":      34,
-		"magenta":   35,
-		"cyan":      36,
-		"gray":      37,
-		"white":     97,
-		"BrBlack":   90,
-		"BrRed":     91,
-		"BrGreen":   92,
-		"BrYellow":  93,
-		"BrBlue":    94,
-		"BrMagenta": 95,
-		"BrCyan":    96,
-		"BrGray":    97,
+// ApplyBlink applies the blink effect to the given message
+func (gs *gostyle) ApplyBlink(msg string) string {
+	return fmt.Sprintf("\x1b[5m%s\x1b[0m", msg) // Start blink, then reset
+}
+
+// ApplyReverse applies the reverse effect to the given message
+func (gs *gostyle) ApplyReverse(msg string) string {
+	return fmt.Sprintf("\x1b[7m%s\x1b[0m", msg) // Start reverse, then reset
+}
+
+// ApplyHidden applies the hidden effect to the given message
+func (gs *gostyle) ApplyHidden(msg string) string {
+	return fmt.Sprintf("\x1b[8m%s\x1b[0m", msg) // Start hidden, then reset
+}
+
+// LogInfo prints an informational message with a yellow prefix
+func (gs *gostyle) LogInfo(msg string) string {
+	return gs.Log("[!] ", "33", msg) // Yellow
+}
+
+// LogQue prints a question message with a cyan prefix
+func (gs *gostyle) LogQue(msg string) string {
+	return gs.Log("[?] ", "36", msg) // Cyan
+}
+
+// LogBad prints a bad message with a red prefix
+func (gs *gostyle) LogBad(msg string) string {
+	return gs.Log("[-] ", "31", msg) // Red
+}
+
+// LogGood prints a good message with a green prefix
+func (gs *gostyle) LogGood(msg string) string {
+	return gs.Log("[+] ", "32", msg) // Green
+}
+
+// LogRun prints a running message with a blue prefix
+func (gs *gostyle) LogRun(msg string) string {
+	return gs.Log("[~] ", "34", msg) // Blue
+}
+
+// Log prints a log message with the given prefix, color code, and message
+func (gs *gostyle) Log(prefix, colorCode, msg string) string {
+	// Construct and return the styled log message
+	return fmt.Sprintf("\x1b[%sm%s%s\x1b[0m", colorCode, prefix, msg)
+}
+
+// Define color functions
+func (gs *gostyle) Black(msg string) string {
+	return fmt.Sprintf("\x1b[30m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Red(msg string) string {
+	return fmt.Sprintf("\x1b[31m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Green(msg string) string {
+	return fmt.Sprintf("\x1b[32m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Yellow(msg string) string {
+	return fmt.Sprintf("\x1b[33m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Blue(msg string) string {
+	return fmt.Sprintf("\x1b[34m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Magenta(msg string) string {
+	return fmt.Sprintf("\x1b[35m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Cyan(msg string) string {
+	return fmt.Sprintf("\x1b[36m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Gray(msg string) string {
+	return fmt.Sprintf("\x1b[37m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) White(msg string) string {
+	return fmt.Sprintf("\x1b[97m%s\x1b[0m", msg)
+}
+
+// Define bright color functions
+func (gs *gostyle) BrBlack(msg string) string {
+	return fmt.Sprintf("\x1b[90m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrRed(msg string) string {
+	return fmt.Sprintf("\x1b[91m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrGreen(msg string) string {
+	return fmt.Sprintf("\x1b[92m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrYellow(msg string) string {
+	return fmt.Sprintf("\x1b[93m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrBlue(msg string) string {
+	return fmt.Sprintf("\x1b[94m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrMagenta(msg string) string {
+	return fmt.Sprintf("\x1b[95m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrCyan(msg string) string {
+	return fmt.Sprintf("\x1b[96m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) BrGray(msg string) string {
+	return fmt.Sprintf("\x1b[97m%s\x1b[0m", msg)
+}
+
+// Define text style functions
+func (gs *gostyle) Bold(msg string) string {
+	return fmt.Sprintf("\x1b[1m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Italic(msg string) string {
+	return fmt.Sprintf("\x1b[3m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Underline(msg string) string {
+	return fmt.Sprintf("\x1b[4m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Dim(msg string) string {
+	return fmt.Sprintf("\x1b[2m%s\x1b[0m", msg)
+}
+
+func (gs *gostyle) Strike(msg string) string {
+	return fmt.Sprintf("\x1b[9m%s\x1b[0m", msg)
+}
+
+// BGround applies background color to the given message
+// Color parameter should be one of the available color strings
+func (gs *gostyle) BGround(msg string, color string) string {
+	var colorCode int
+	switch color {
+	case "black":
+		colorCode = 40
+	case "red":
+		colorCode = 41
+	case "green":
+		colorCode = 42
+	case "yellow":
+		colorCode = 43
+	case "blue":
+		colorCode = 44
+	case "magenta":
+		colorCode = 45
+	case "cyan":
+		colorCode = 46
+	case "white":
+		colorCode = 47
+	case "BrBlack":
+		colorCode = 90
+	case "BrRed":
+		colorCode = 91
+	case "BrGreen":
+		colorCode = 92
+	case "BrYellow":
+		colorCode = 93
+	case "BrBlue":
+		colorCode = 94
+	case "BrMagenta":
+		colorCode = 95
+	case "BrCyan":
+		colorCode = 96
+	case "BrWhite":
+		colorCode = 97
+	default:
+		return msg // Return message as is if color not recognized
 	}
-
-	styleMap = map[string]int{
-		"bold":      1,
-		"dim":       2,
-		"italic":    3,
-		"underline": 4,
-		"strike":    9,
-	}
-
-	effectMap = map[string]string{
-		"blink":   "\x1b[5m",
-		"reverse": "\x1b[7m",
-		"hidden":  "\x1b[8m",
-		"reset":   "\x1b[0m",
-	}
-
-	logTypeMap = map[string]struct {
-		Prefix string
-		Color  func(string) string
-	}{
-		"info": {Prefix: "[!] ", Color: New().Yellow},
-		"que":  {Prefix: "[?] ", Color: New().Cyan},
-		"bad":  {Prefix: "[-] ", Color: New().Red},
-		"good": {Prefix: "[+] ", Color: New().Green},
-		"run":  {Prefix: "[~] ", Color: New().Blue},
-	}
-)
-
-// ApplyEffect applies the specified text effect to the given message
-// Currently it includes effects such as blink, reversed, hidden.
-func (gs *GoStyle) ApplyEffect(effect, msg string) string {
-	code, ok := effectMap[effect]
-	if !ok {
-		fmt.Println("Invalid effect")
-		return msg
-	}
-	return fmt.Sprintf("%s%s%s", code, msg, effectMap["reset"])
-}
-
-// Log prints a log message with the given type and message
-func (gs *GoStyle) Log(logType, msg string) string {
-	log, ok := logTypeMap[logType]
-	if !ok {
-		fmt.Println("Invalid log type")
-		return ""
-	}
-	return fmt.Sprintf("%s%s", log.Color(log.Prefix), msg)
-}
-
-// Info prints an informational message
-func (gs *GoStyle) Info(msg string) string {
-	return gs.Log("info", msg)
-}
-
-// Que prints a question message
-func (gs *GoStyle) Que(msg string) string {
-	return gs.Log("que", msg)
-}
-
-// Bad prints a bad message
-func (gs *GoStyle) Bad(msg string) string {
-	return gs.Log("bad", msg)
-}
-
-// Good prints a good message
-func (gs *GoStyle) Good(msg string) string {
-	return gs.Log("good", msg)
-}
-
-// Run prints a running message
-func (gs *GoStyle) Run(msg string) string {
-	return gs.Log("run", msg)
-}
-
-// Black applies black color to the given message
-func (gs *GoStyle) Black(msg string) string {
-	return gs.color(msg, "black")
-}
-
-// Red applies red color to the given message
-func (gs *GoStyle) Red(msg string) string {
-	return gs.color(msg, "red")
-}
-
-// Green applies green color to the given message
-func (gs *GoStyle) Green(msg string) string {
-	return gs.color(msg, "green")
-}
-
-// Blue applies blue color to the given message
-func (gs *GoStyle) Blue(msg string) string {
-	return gs.color(msg, "blue")
-}
-
-// Yellow applies yellow color to the given message
-func (gs *GoStyle) Yellow(msg string) string {
-	return gs.color(msg, "yellow")
-}
-
-// Magenta applies magenta color to the given message
-func (gs *GoStyle) Magenta(msg string) string {
-	return gs.color(msg, "magenta")
-}
-
-// Cyan applies cyan color to the given message
-func (gs *GoStyle) Cyan(msg string) string {
-	return gs.color(msg, "cyan")
-}
-
-// Gray applies gray color to the given message
-func (gs *GoStyle) Gray(msg string) string {
-	return gs.color(msg, "gray")
-}
-
-// White applies white color to the given message
-func (gs *GoStyle) White(msg string) string {
-	return gs.color(msg, "white")
-}
-
-// BrBlue applies bright blue color to the given message
-func (gs *GoStyle) BrBlue(msg string) string {
-	return gs.color(msg, "BrBlue")
-}
-
-// BrRed applies bright red color to the given message
-func (gs *GoStyle) BrRed(msg string) string {
-	return gs.color(msg, "BrRed")
-}
-
-// BrGreen applies bright green color to the given message
-func (gs *GoStyle) BrGreen(msg string) string {
-	return gs.color(msg, "BrGreen")
-}
-
-// BrYellow applies bright yellow color to the given message
-func (gs *GoStyle) BrYellow(msg string) string {
-	return gs.color(msg, "BrYellow")
-}
-
-// BrBlack applies bright black color to the given message
-func (gs *GoStyle) BrBlack(msg string) string {
-	return gs.color(msg, "BrBlack")
-}
-
-// BrMagenta applies bright magenta color to the given message
-func (gs *GoStyle) BrMagenta(msg string) string {
-	return gs.color(msg, "BrMagenta")
-}
-
-// BrCyan applies bright cyan color to the given message
-func (gs *GoStyle) BrCyan(msg string) string {
-	return gs.color(msg, "BrCyan")
-}
-
-// BrGray applies bright gray color to the given message
-func (gs *GoStyle) BrGray(msg string) string {
-	return gs.color(msg, "BrGray")
-}
-
-// Bold applies bold style to the given message
-func (gs *GoStyle) Bold(msg string) string {
-	return gs.style(msg, "bold")
-}
-
-// Italic applies italic style to the given message
-func (gs *GoStyle) Italic(msg string) string {
-	return gs.style(msg, "italic")
-}
-
-// Underline applies underline style to the given message
-func (gs *GoStyle) Underline(msg string) string {
-	return gs.style(msg, "underline")
-}
-
-// Dim applies dim style to the given message
-func (gs *GoStyle) Dim(msg string) string {
-	return gs.style(msg, "dim")
-}
-
-// Strike applies strike style to the given message
-func (gs *GoStyle) Strike(msg string) string {
-	return gs.style(msg, "strike")
-}
-
-// BGround applies bright background color to the given message
-func (gs *GoStyle) BGround(msg string, color string) string {
-	return gs.bg(msg, color)
-}
-
-// color applies color to the given message
-func (gs *GoStyle) color(msg string, color string) string {
-	colorCode := colorMap[color]
 	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorCode, msg)
-}
-
-// bg applies background color to the given message
-func (gs *GoStyle) bg(msg string, color string) string {
-	colorCode := colorMap[color] + 10 // Background color codes are offset by 10
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", colorCode, msg)
-}
-
-// style applies style to the given message
-func (gs *GoStyle) style(msg string, styleStr string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", styleMap[styleStr], msg)
 }
